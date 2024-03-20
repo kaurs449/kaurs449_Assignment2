192f8d5a0a992850ef0365f9736ff806694ecc71
kaurs449 <kaurs449@SCE002>
2024-03-19 18:43:06 -0600

    Add project files.

---
+++ kaurs449_Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+        {
+            Console.WriteLine($"{i} is an even number");
+            evenSum += i;
+        } 
+        else
+        {
+            Console.WriteLine($"{i} is an odd number");
+            oddSum += i;
+        }
+    }
+         Console.WriteLine($"Total of even numbers: {evenSum}");
+         Console.WriteLine($"Total of odd numbers: {oddSum}");
+        }
+  }
+
